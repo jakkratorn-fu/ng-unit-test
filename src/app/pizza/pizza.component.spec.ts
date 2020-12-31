6fc10cd34f1de937948a37230d70b92d7d798382
jakkratornpha <jakkratorn.phanchaweng@allianz.com>
2020-12-31 17:20:03 +0700

    fix bug

--- src/app/pizza/pizza.component.spec.ts
+++ src/app/pizza/pizza.component.spec.ts
@@ -1,3 +1,4 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PizzaComponent } from './pizza.component';
@@ -5,12 +6,13 @@ import { PizzaComponent } from './pizza.component';
 describe('PizzaComponent', () => {
   let component: PizzaComponent;
   let fixture: ComponentFixture<PizzaComponent>;
+  let de: DebugElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ PizzaComponent ]
     })
-    .compileComponents();
+    .compileComponents(); // compile template and css
   });
 
   beforeEach(() => {
@@ -23,28 +25,28 @@ describe('PizzaComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it(`should have a title 'I love pizza!'`, (() => {
-    fixture = TestBed.createComponent(PizzaComponent);
-    component = fixture.debugElement.componentInstance;
-    expect(component.title).toEqual('I love pizza!');
-  }));
+  // it(`should have a title 'I love pizza!'`, () => {
+  //   fixture = TestBed.createComponent(PizzaComponent);
+  //   component = fixture.debugElement.componentInstance;
+  //   expect(component.title).toEqual('I love pizza!');
+  // });
   
-  it('#clicked() should toggle #isOn', () => {
-    fixture = TestBed.createComponent(PizzaComponent);
-    let comp = fixture.debugElement.componentInstance;
-    expect(comp.isOn).toBe(false, 'off at first');
-    comp.clicked();
-    expect(comp.isOn).toBe(true, 'on after click');
-    comp.clicked();
-    expect(comp.isOn).toBe(false, 'off after second click');
-  });
+  // it('#clicked() should toggle #isOn', () => {
+  //   fixture = TestBed.createComponent(PizzaComponent);
+  //   let comp = fixture.debugElement.componentInstance;
+  //   expect(comp.isOn).toBe(false, 'off at first');
+  //   comp.clicked();
+  //   expect(comp.isOn).toBe(true, 'on after click');
+  //   comp.clicked();
+  //   expect(comp.isOn).toBe(false, 'off after second click');
+  // });
 
-  it('#clicked() should set #message to "is on"', () => {
-    fixture = TestBed.createComponent(PizzaComponent);
-    let comp = fixture.debugElement.componentInstance;
-    expect(comp.message).toMatch(/is off/i, 'off at first');
-    comp.clicked();
-    expect(comp.message).toMatch(/is on/i, 'on after clicked');
-  });
+  // it('#clicked() should set #message to "is on"',() => {
+  //   fixture = TestBed.createComponent(PizzaComponent);
+  //   let comp = fixture.debugElement.componentInstance;
+  //   expect(comp.message).toMatch(/is off/i, 'off at first');
+  //   comp.clicked();
+  //   expect(comp.message).toMatch(/is on/i, 'on after clicked');
+  // });
 
 });
--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -9,33 +9,9 @@ import { TestBed } from '@angular/core/testing';
 export class PizzaComponent implements OnInit {
   title: String = "I love pizza!"
   isOn = false;
-  fixture = TestBed.createComponent(PizzaComponent);
-  
-  // clicked() { this.isOn = !this.isOn; }
-  // get message() { return `The light is ${this.isOn ? 'On' : 'Off'}`; }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  it('#clicked() should toggle #isOn',as () => {
-    const fixture = TestBed.createComponent(PizzaComponent);
-    let comp = fixture.debugElement.componentInstance;
-    expect(comp.isOn).toBe(false, 'off at first');
-    comp.clicked();
-    expect(comp.isOn).toBe(true, 'on after click');
-    comp.clicked();
-    expect(comp.isOn).toBe(false, 'off after second click');
-  });
-
-  // it('#clicked() should set #message to "is on"', () => {
-  //   const fixture = TestBed.createComponent(PizzaComponent);
-  //   let comp = fixture.debugElement.componentInstance;
-  //   expect(comp.message).toMatch(/is off/i, 'off at first');
-  //   comp.clicked();
-  //   expect(comp.message).toMatch(/is on/i, 'on after clicked');
-  // });
-
-
 }
